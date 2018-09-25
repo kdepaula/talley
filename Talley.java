883045a319fe91e5b5d8860955bf2c359f6b58df
kdepaula <42978472+kdepaula@users.noreply.github.com>
2018-09-25 14:29:07 -0400

    Add files via upload

---
+++ Talley.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * @author kdepaula20
+ *
+ */
+public class Talley 
+{
+	//Instance Variables
+	
+	private int count;
+	
+	//Constructor
+	
+	public Talley()
+	{
+		count = 0;
+	}
+	
+	public Talley(int startNum)
+	{
+		count = startNum;
+		//this means you can start the Talley counter at any number you want
+	}
+	
+	public void click()
+	{
+		count = count + 1;
+		//count++; (this is a shortcut for count = count +1;
+		//count+=1; (this is a shortcut to add any number)
+		//you can also do multiply, divide, or subtract instead of +
+	}
+}
